dbe108ed5b6eee8cd02e4eae390dbe9812631112
Nathan <nathanstouffer1999@gmail.com>
2020-06-23 20:01:44 -0600

    edited time-grapher

--- exploring-options/JavaTest/time-grapher.py
+++ exploring-options/JavaTest/time-grapher.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # original data
-num_particles = [ 75, 100, 150, 200, 250, 300, 350, 400,  450,  500 ]
-times         = [ 49,  76, 149, 235, 348, 524, 698, 835, 1094, 1385 ]
+num_agents = [ 75, 100, 150, 200, 250, 300, 350, 400,  450,  500 ]
+times      = [ 49,  76, 149, 235, 348, 524, 698, 835, 1094, 1385 ]
 
 # we now fit a curve to the data
 # parabola coefficients
@@ -17,10 +17,10 @@ eq = a + b*n + c*n*n
 # set up plot
 plt.title("Simulation time (to reach 100000 iterations) vs Number of Agents")
 plt.xlabel("Number of agents")
-plt.ylabel("Time to reach 100000 movement iterations")
+plt.ylabel("Seconds to reach 100000 movement iterations")
 
 # plot data
-plt.scatter(num_particles, times)
+plt.scatter(num_agents, times)
 # plot curve
 plt.plot(n, eq)
 
