5b14ca9e2780e75f4fe2719ac92cf15c2670d28a
Dominique Zosso <dominique.zosso@montana.edu>
2020-07-13 16:16:40 -0600

    Add ODEwrapper.m that contains all of the setup; reduce janus to just computing things on the fly.
    "fixed" advection term: the differentiation needs to happen along different dimensions, and product between f and rho needs to be pointwise.

--- MATLAB/janus.m
+++ MATLAB/janus.m
@@ -1,39 +1,33 @@
-function dy = janus(y,t)
+function dy = janus(y, N, phi, sigma, v, D_phi, D_xy, K, D, L)
     %Model for Janus Particles
     %   
     
-    % Some parameters we can vary
-    sigma = 4; % Activation Threshold
-    N = 32; % Resolution
-    phi = 100; % Number of Angles
-    
     % Some information about angle species
-    step = 2*pi/phi;
-    theta = 0:step:2*pi-step; % This vector has the angle amount for each species
+    theta = linspace(0,2*pi, phi+1);
     
-    % Setting up L and div matrices
-    Df = spdiags( repmat( [-1 1], N,1 ), [0 1], N, N ); % This is a sparse, NxN matrix where  +/- ones(N,1) are placed on the 0th and 1st diagonal
-    Df(end,:) = 0;
-    Db = -rot90(Df,2);
-    div = -Df'; % Sets up divergence matrix for advection term
-    L = (-Df'*Df-Db'*Db)/2; % Laplacian is average of fwd/bwd grad-divergence composition
-    
+
     % Shaping things and setting up arrays
-    rho_stack = reshape(y,N,N,phi);
+    rho_stack = reshape(y,[N,N,phi]);
     rho = squeeze(num2cell(rho_stack,[1 2]));
-    K = Klibfunc(pi/4,phi,N);
     rho_int = sum(cat(3,rho{:}),3);
     
-    dy = cell(1,phi);
+    dy = cell(phi,1);
     for i = 1:phi 
-        rho_xx = L*rho{i} + rho{i}*L; % The Laplacian Term
-        rho_thetatheta = rho{mod(i-2,phi)+1} - 2*rho{i} + rho{mod(i,phi)+1}; % The Angular Diffusion term
+        % The Laplacian Term in space
+        rho_xx = L*rho{i} + rho{i}*L; 
+        
+        % The Angular Diffusion term
+        rho_thetatheta = rho{mod(i-2,phi)+1} - 2*rho{i} + rho{mod(i,phi)+1}; 
         
         %Advection Term
-        V = conv2(rho_int,full(K{i}),'same'); % Convolution
+        V = conv2(rho_int,K{i},'same'); % Convolution
         f = (V >= sigma); % Activation
-        A = div*f*rho{i}*cos(theta(i)) + div*f*rho{i}*sin(theta(i)); 
-        dy{i} = rho_xx - A + rho_thetatheta;
+        
+        A = D*(f.*rho{i})*cos(theta(i)) + (f.*rho{i})*D*sin(theta(i)); 
+        
+        
+        % putting it all together
+        dy{i} = rho_xx*D_xy - A*v + rho_thetatheta*D_phi;
     end
     
     dy_stack = cat(3,dy{:});
