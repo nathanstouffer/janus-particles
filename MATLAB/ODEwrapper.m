fedb418d51018ecd281a88aaa947d7f36bc59dcf
Dominique Zosso <dominique.zosso@montana.edu>
2020-07-14 17:28:35 -0600

    reduced angular resolution and included option for stiff solver. TBD whether that is any good.

--- MATLAB/ODEwrapper.m
+++ MATLAB/ODEwrapper.m
@@ -2,13 +2,15 @@ clear all;
 close all;
 clc;
 
+stiff = false;  % only set to true when working on meriwether... maybe?
+
 R = 250;  % 250 um side length of the 0-1-square
 N = 32; % spatial resolution
 h_x = R/N; % spatial step in um
 
-phi = 36; % angular resolution
+phi = 16; % angular resolution
 
-T = 10000;  % simulation duration
+T = 200;  % simulation duration
 
 n = 75; % "number" of particles --- total initial density
 alpha = pi/4;
@@ -39,7 +41,11 @@ L = (-Df'*Df-Db'*Db)/2; % Laplacian is average of fwd/bwd grad-divergence compos
 % setting up kernel libraries
 K = Klibfunc(alpha,phi,N);
 
-[t,y] = ode113(@(t,y) janus(y,N,phi,Pstar,v,D_phi,D_xy,K,D,L), (0:0.2:1)*T, rhostack(:));
+if stiff
+    [t,y] = ode15s(@(t,y) janus(y,N,phi,Pstar,v,D_phi,D_xy,K,D,L), (0:0.2:1)*T, rhostack(:));
+else
+    [t,y] = ode113(@(t,y) janus(y,N,phi,Pstar,v,D_phi,D_xy,K,D,L), (0:0.2:1)*T, rhostack(:));
+end
 
 
 %% viz: final perception and activations
@@ -47,12 +53,13 @@ K = Klibfunc(alpha,phi,N);
 figure;
 stack = reshape(y(end,:),[N,N,phi]);
 rho_int = sum(stack,3);
+r = sqrt(phi);
 for i = 1:phi
     P = conv2(rho_int,K{i},'same'); % Convolution
     f = (P >= Pstar); % Activation
-    subplot(6,12,i);
+    subplot(r,2*r,i);
     imagesc(P);
-    subplot(6,12,36+i);
+    subplot(r,2*r,phi+i);
     imagesc(1*f);
 end
  
@@ -63,7 +70,7 @@ for tt = 1:length(t)
     stack = reshape(y(tt,:),[N,N,phi]);
     phistep = 3;
     for p = 1:phistep:phi
-        subplot(length(t),phi/phistep,(1+(p-1)/phistep+(tt-1)*phi/phistep));
+        subplot(length(t),phi/phistep+1,(1+(p-1)/phistep+(tt-1)*(phi/phistep+1)));
         imagesc(stack(:,:,p));
         title("t = " + t(tt) + "s, phi = " + p);
         axis tight;
@@ -71,4 +78,11 @@ for tt = 1:length(t)
         axis off;
         box off;
     end
+    subplot(length(t),phi/phistep+1,tt*(phi/phistep+1));
+        imagesc(sum(stack,3));
+        axis tight;
+        axis equal;
+        axis off;
+        box off;
+    disp("at t = " + t(tt) + "s: n = " + (sum(stack,'all')) );
 end
