9451d67a85d290cdd7e4d57783b23e9ce68ce42b
Dominique Zosso <dominique.zosso@montana.edu>
2020-07-28 18:19:08 -0600

    Playing around with visualizations and angular resolution. There is a sign mistake, somewhere (it seems to work in y direction, but spreads in x direction....)

--- MATLAB/ODEwrapper.m
+++ MATLAB/ODEwrapper.m
@@ -8,23 +8,24 @@ R = 250;  % 250 um side length of the 0-1-square
 N = 32; % spatial resolution
 h_x = R/N; % spatial step in um
 
-phi = 36; % angular resolution
+phi = 4; % angular resolution
 
-T = 2500;  % simulation duration
+T = 50000;  % simulation duration
 
 n = 75; % "number" of particles --- total initial density
 alpha = pi/4;
 
-Pstar = 2*alpha*n/pi/pi/N; % activation threshold
-v = 0.4; % activation drift (um/s)
+Pstar = alpha*n/pi/pi/N; % activation threshold
+v = 0.2; % activation drift (um/s)
 D_xy = 0.02; % spatial diffusion coefficient (um^2/s)
 D_phi = 1/110; % angular diffusion coefficient (1/s)
 
 
 
 
-rhostack = zeros(N,N,phi);
-rhostack( randperm(N*N*phi,n) ) = 1; % place n random particles...
+rhostack = zeros(N/2,N/2,phi);
+rhostack( randperm(N/2*N/2*phi,n) ) = 1; % place n random particles...
+rhostack = padarray(rhostack, [N/4 N/4 0], 0, 'both');
 
 % Setting up L and div matrices
 % forward differences
@@ -42,7 +43,7 @@ L = (-Df'*Df-Db'*Db)/2; % Laplacian is average of fwd/bwd grad-divergence compos
 % setting up kernel libraries
 K = Klibfunc(alpha,phi,N);
 
-tspan = (0:0.02:1)*T;
+tspan = (0:0.01:1)*T;
 
 options = odeset('OutputFcn',@(t,y,flag) MyOutputFcn(t,y,flag,tspan(end)));
 
@@ -73,11 +74,13 @@ end
 %% viz: densities over time
 
 figure;
-for tt = 1:length(t)
+phistep = 1;
+tstep = 10;
+for tt = 1:tstep:length(t)
     stack = reshape(y(tt,:),[N,N,phi]);
-    phistep = 3;
+    
     for p = 1:phistep:phi
-        subplot(length(t),phi/phistep+1,(1+(p-1)/phistep+(tt-1)*(phi/phistep+1)));
+        subplot(ceil(length(t)/tstep),phi/phistep+1,(1+(p-1)/phistep+floor(tt/tstep)*(phi/phistep+1)));
         imagesc(stack(:,:,p));
         title("t = " + t(tt) + "s, phi = " + p);
         axis tight;
@@ -85,13 +88,19 @@ for tt = 1:length(t)
         axis off;
         box off;
     end
-    subplot(length(t),phi/phistep+1,tt*(phi/phistep+1));
+    subplot(ceil(length(t)/tstep),phi/phistep+1,ceil(tt/tstep)*(phi/phistep+1));
     imagesc(sum(stack,3));
     axis tight;
     axis equal;
     axis off;
     box off;
     disp("at t = " + t(tt) + "s: n = " + (sum(stack,'all')) );
+end
+
+%% movie
+figure;
+for tt = 1:length(t)
+    imagesc(sum(reshape(y(tt,:),[N,N,phi]),3)); drawnow; pause(0.1);
 end
 
 
