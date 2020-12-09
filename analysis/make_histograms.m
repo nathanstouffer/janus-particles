c1ed758ccc662fdc7938b207fcba947d869bfeef
Nathan Stouffer <nathanstouffer1999@gmail.com>
2020-12-09 13:55:51 -0700

    hopefully finished the make histograms script

--- analysis/make_histograms.m
+++ analysis/make_histograms.m
@@ -1,8 +1,10 @@
-function make_hist(in_dir)
+function make_histograms(in_dir)
 
-    d = dir('in_dir').name
-    dfolder = d([d(:).isdir])
-    
-    densityviz(in_dir);
-    
+    d = dir(in_dir);
+    for i = 3:numl(d)       % start at 3 to skip . and ..
+        if (d(i).isdir)
+            densityviz(in_dir);
+        end
+    end
+
 end
