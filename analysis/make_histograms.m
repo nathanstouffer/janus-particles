e8144b412e3309a3bef97f9482a46ecf75c2bbb0
Griffin Smith <21smithg>
2020-12-09 15:00:41 -0700

    make histograms fixed error

--- analysis/make_histograms.m
+++ analysis/make_histograms.m
@@ -1,7 +1,7 @@
 function make_histograms(in_dir)
 
     d = dir(in_dir);
-    for i = 3:numl(d)       % start at 3 to skip . and ..
+    for i = 3:numel(d)       % start at 3 to skip . and ..
         if (d(i).isdir)
             densityviz(in_dir);
         end
