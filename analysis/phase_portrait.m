cce0a77412ffc9a316680f77283f4a7d208667fa
Nathan Stouffer <nathanstouffer1999@gmail.com>
2020-12-15 13:23:21 -0700

    working on the script to make phase portraits

--- analysis/phase_portrait.m
+++ analysis/phase_portrait.m
@@ -1,4 +1,4 @@
-function [S] = phase_portrait(directory_name)
+function phase_portrait(directory_name)
 %Draws a phase portrait from the .mat files in the desired directory
 %   Input a directory name with only .mat files in it
 
@@ -9,6 +9,10 @@ S = zeros(1,n-2); % Empty entropy vector
 
 A = zeros(1,n-2);
 P = zeros(1,n-2);
+
+res = 32;
+disc_alph = linspace(pi/res,pi,res);
+disc_perc = linspace(1.2/res,1.2,res);
 
 for k = 3:n
     fin = X(k).name;
@@ -24,8 +28,10 @@ for k = 3:n
     percep = percep(1);
     percep = str2num(percep{1});
     
+    p_scale = (2*alpha*75/(pi*pi));
+    
     A(k-2) = alpha;
-    P(k-2) = percep;
+    P(k-2) = percep/p_scale;
     
     S(k-2) = entropy(strcat(directory_name,"/",X(k).name));
     
@@ -35,12 +41,15 @@ end
 figure;
 plot3(A,P,S,'x');
 
-A = unique(A);
-P = unique(P);
-
 figure;
-heatmap = reshape(S,sqrt(n-2),sqrt(n-2));
-surf(A,P,heatmap);
+heatmap = zeros(sqrt(n-2),sqrt(n-2));
+for k=1:length(A)
+    i = closest_index(disc_perc, P(k), 0.001);
+    j = closest_index(disc_alph, A(k), 0.001);
+    heatmap(i,j) = S(k);
+end
+
+im = imagesc(heatmap);
 
 title('Entropy')
 xlabel('Alpha')
@@ -49,3 +58,11 @@ zlabel('Entropy')
 
 end
 
+function j = closest_index(vec, val, eps)
+    j = 0
+    for i=1:length(vec)
+        if abs(vec(i) - val) < eps
+            j = i;
+        end
+    end
+end
