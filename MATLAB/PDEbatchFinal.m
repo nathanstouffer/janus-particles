9780eeab8f0afbbac60824676d2f618ab5c50696
Griffin Smith <21smithg>
2021-03-09 19:47:29 -0700

    The real deal

--- MATLAB/PDEbatchFinal.m
+++ MATLAB/PDEbatchFinal.m
@@ -2,22 +2,22 @@
 
 
 n = 75; % Initial particle density
-disc = 16; % Discritization
+disc = 32; % Discritization
 
 alpha = linspace(pi/disc,pi,disc);
 
 
 for i = 1:disc
     
-    p_c_alpha = 2*alpha(i)*n/pi/pi/32;
+    p_c_alpha = 2*alpha(i)*n/pi/pi/64;
     
     perception = linspace(p_c_alpha/disc,(1.2)*p_c_alpha,disc);
 
     for k = 1:disc
     
-        X = PDEsimulation(alpha(i),perception(k),16,6);
+        X = PDEsimulation(alpha(i),perception(k),64,60);
 
-        file_name = strcat('../data/phase-portrait/pde/sample-run/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
+        file_name = strcat('../data/phase-portrait/pde/thumbnail-data/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
 
         save(file_name,'X');
     end
