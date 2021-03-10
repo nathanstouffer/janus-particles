df21bbeb743047aaa47d92b412f89efc8e7616d7
Griffin Smith <21smithg>
2021-03-09 19:21:02 -0700

    For a test run of the PDEbatch file

--- MATLAB/PDEbatchFinal.m
+++ MATLAB/PDEbatchFinal.m
@@ -2,12 +2,12 @@
 
 
 n = 75; % Initial particle density
-disc = 32; % Discritization
+disc = 16; % Discritization
 
 alpha = linspace(pi/disc,pi,disc);
 
 
-parfor i = 1:disc
+for i = 1:disc
     
     p_c_alpha = 2*alpha(i)*n/pi/pi/32;
     
@@ -15,9 +15,9 @@ parfor i = 1:disc
 
     for k = 1:disc
     
-        X = PDEsimulation(alpha(i),perception(k));
+        X = PDEsimulation(alpha(i),perception(k),16,6);
 
-        file_name = strcat('../data/phase-portrait/pde/weird-scaling/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
+        file_name = strcat('../data/phase-portrait/pde/sample-run/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
 
         save(file_name,'X');
     end
