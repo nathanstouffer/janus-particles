69a8c057fc37de6a866127c1dbb95ed94f4d722a
Griffin Smith <21smithg>
2021-03-15 21:22:02 -0600

    Third Pass

--- MATLAB/activation_analysis.m
+++ MATLAB/activation_analysis.m
@@ -6,7 +6,7 @@ function [Activation] = activation_analysis(angular_data, alpha)
 [n,m,z] = size(angular_data);
 
 % Need to know alpha and the spatial and angular resolution for Kernels
-K = Klibfunc(alpha,m,z);
+K = Klibfunc(alpha,z,m);
 
 % Let's hard-code the threshold (Usually using P_c^alpha with 75 particles)
 P = 2*alpha*75/pi/pi/m;
@@ -17,6 +17,7 @@ rho_int = sum(angular_data,3);
 % pad rho_int to reach 4*N side length, fft2
 rho_int_hat = fft2(padarray(rho_int, [3*m 3*m], 0, 'post'));
 
+siz
 % Initialize array to store the activation values
 Activation = zeros(n,n,z);
 for i = 1:z
