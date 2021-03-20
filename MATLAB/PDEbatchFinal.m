75b5bab8c1cf49825492cd39f176e882a4ac1884
Griffin Smith <21smithg>
2021-03-20 13:10:52 -0600

    Fixing error

--- MATLAB/PDEbatchFinal.m
+++ MATLAB/PDEbatchFinal.m
@@ -10,13 +10,13 @@ for i = 1:disc
     
     p_c_alpha = 2*alpha(i)*n/pi/pi/64;
     
-    perception = [p_c_alpha/20:p_c_alpha:1.2*(p_c_alpha)];
+    perception = [p_c_alpha/20:p_c_alpha/20:1.2*(p_c_alpha)];
 
     for k = 1:disc
     
         X = PDEsimulation(alpha(i),perception(k),64,30);
 
-        file_name = strcat('../data/phase-portrait/pde/thumbnail-roundtwo/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
+        file_name = strcat('../data/phase-portrait/pde/thumbnail-roundthree/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
 
         save(file_name,'X');
         
