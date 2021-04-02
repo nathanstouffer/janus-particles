5126c2046272d21ec058c6fccf55db33b709b060
Griffin Smith <21smithg>
2021-04-02 12:11:34 -0600

    A test of cohesion portrait script

--- MATLAB/cohesion_portrait.m
+++ MATLAB/cohesion_portrait.m
@@ -2,26 +2,26 @@
 
 
 n = 75; % Initial particle density
-disc = 24; % Discritization
+disc = 10; % Discritization
 
 
 alpha = linspace(pi/disc,pi,disc); % Alpha spacing
 
-initial = load('cohesion_start_data.mat');
+initial = load('cohesion_start16_data.mat');
 initial = cell2mat(struct2cell(initial));
 
 
 for i = 1:disc
     
-    p_c_alpha = 2*alpha(i)*n/pi/pi/64;
+    p_c_alpha = 2*alpha(i)*n/pi/pi/16;
     
     perception = [p_c_alpha/20:p_c_alpha/20:1.2*(p_c_alpha)];
 
     for k = 1:disc
     
-        [A,X] = PDEsimulation_hotstart(alpha(i),perception(k),64,30,initial);
+        [A,X] = PDEsimulation_hotstart(alpha(i),perception(k),16,30,initial);
 
-        file_name = strcat('../data/phase-portrait/pde/cohesion/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
+        file_name = strcat('../data/phase-portrait/pde/cohesion_test/PDESim-alpha-',num2str(alpha(i)),'-percep-',num2str(perception(k)),'.mat');
 
         save(file_name,'X');
         
