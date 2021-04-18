e6d48aecce79aa4e48599738e2cac16b1626d909
Griffin Smith <21smithg>
2021-04-17 22:58:37 -0600

    Error

--- MATLAB/angular_convergence.m
+++ MATLAB/angular_convergence.m
@@ -2,7 +2,7 @@
 
 angles = [4,8,15,30,60,120];
 for i = 1:6
-    [A,X] = PDEsimulation_angle(pi/2,2*(pi/2)*n/pi/pi/64,128,angles(i));
+    [A,X] = PDEsimulation_angle(pi/2,2*(pi/2)*75/pi/pi/64,128,angles(i));
     filename = strcat('angular_convergence',num2str(angles(i)),'.mat');
     save(filename,'X')
     filename = strcat('angles_',filename);
