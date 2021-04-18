0a8999b4eedc8da48cc5cb38f049f00d83d7e7a5
Griffin Smith <21smithg>
2021-04-17 22:56:32 -0600

    Angular Convergence Script

---
+++ MATLAB/angular_convergence.m
@@ -0,0 +1,10 @@
+%% Angular Convergence Data Generation
+
+angles = [4,8,15,30,60,120];
+for i = 1:6
+    [A,X] = PDEsimulation_angle(pi/2,2*(pi/2)*n/pi/pi/64,128,angles(i));
+    filename = strcat('angular_convergence',num2str(angles(i)),'.mat');
+    save(filename,'X')
+    filename = strcat('angles_',filename);
+    save(filename,'A')
+end
